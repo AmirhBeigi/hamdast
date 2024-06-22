a361055a8d746f8a6ec16f3a5940c05e934f234b
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-06-22 13:22:20 +0330

    feat: add profile, menus, apps apis

---
+++ src/pages/api/v1/apps/index.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { pb } from "../../../../../pocketbase";
+import config from "next/config";
+const { publicRuntimeConfig } = config();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const cookieStore = req.cookies;
+  const token = (cookieStore["token"] as string) || "";
+  await pb.admins.authWithPassword(
+    publicRuntimeConfig.POCKETBASE_USER_NAME,
+    publicRuntimeConfig.POCKETBASE_PASSWORD
+  );
+  const paziresh24User = await axios.get(
+    "https://apigw.paziresh24.com/v1/auth/me",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const user = paziresh24User.data?.users[0];
+
+  const record = await pb
+    .collection("users")
+    .getFirstListItem(`paziresh24_user_id="${user.id}"`, {
+      expand: "role",
+    });
+
+  const apps = await pb.collection("apps").getFullList({
+    filter: `collaborators~'${record.id}'`,
+  });
+
+  res.status(200).json(
+    apps.map((app) => ({
+      id: app.id,
+      collaborators: app.collaborators,
+      key: app.key,
+      name_en: app.name_en,
+      name_fa: app.name_fa,
+    }))
+  );
+}
---
+++ src/pages/api/v1/profile/index.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { pb } from "../../../../../pocketbase";
+import config from "next/config";
+const { publicRuntimeConfig } = config();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const cookieStore = req.cookies;
+  const token = (cookieStore["token"] as string) || "";
+  await pb.admins.authWithPassword(
+    publicRuntimeConfig.POCKETBASE_USER_NAME,
+    publicRuntimeConfig.POCKETBASE_PASSWORD
+  );
+  const paziresh24User = await axios.get(
+    "https://apigw.paziresh24.com/v1/auth/me",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const user = paziresh24User.data?.users[0];
+
+  const record = await pb
+    .collection("users")
+    .getFirstListItem(`paziresh24_user_id="${user.id}"`, {
+      expand: "role",
+    });
+
+  console.log(token);
+
+  res.status(200).json({
+    id: record.id,
+    paziresh24_user_id: user.id,
+    name: user.name,
+    family: user.family,
+    roles:
+      record.expand?.role?.map?.((item: any) => ({
+        id: item.id,
+        name: item.name,
+      })) ?? [],
+  });
+}
