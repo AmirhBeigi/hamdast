a5ae64807dfc9711d1a8ca95f140a0f7955279cc
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-06-23 12:58:10 +0330

    feat: add hamdast script

---
+++ public/static/js/scripts/hamdast.js
@@ -0,0 +1,55 @@
+(function () {
+  var head = document.getElementsByTagName("head")[0];
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.async = true;
+  script.src =
+    "https://cdn.jsdelivr.net/npm/@rrweb/record@latest/dist/record.umd.min.cjs";
+  head.appendChild(script);
+  var packScript = document.createElement("script");
+  packScript.type = "text/javascript";
+  packScript.async = true;
+  packScript.src =
+    "https://cdn.jsdelivr.net/npm/@rrweb/packer@2.0.0-alpha.15/dist/pack.umd.min.cjs";
+  head.appendChild(packScript);
+})();
+
+function portalCommunication({ clientKey, event }) {
+  const message = JSON.stringify({
+    clientKey,
+    event,
+  });
+  window.parent.postMessage(message, "*");
+}
+
+window.hamdast = {
+  initialize(clientKey) {
+    portalCommunication({ clientKey, event: "INITIALIZE" });
+    window.hamdast?.replay?.record();
+  },
+  replay: {
+    record() {
+      rrweb.record({
+        emit(event) {
+          window.hamdast?.replay?.events?.push?.(event);
+        },
+        packFn: pack,
+        sampling: {
+          input: "last",
+          scroll: 150,
+          media: 800,
+        },
+      });
+
+      // save events every 10 seconds
+      setInterval(windiw.hamdast?.replay?.save, 10 * 1000);
+    },
+    save() {
+      function save() {
+        const body = JSON.stringify({ events: window.hamdast?.replay?.events });
+        console.log("save");
+      }
+    },
+    events: [],
+  },
+};
