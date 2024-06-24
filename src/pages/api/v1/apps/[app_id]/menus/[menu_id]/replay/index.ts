3058a06efd2852d1c27f8b47aca94b3bac8f21c1
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-06-24 14:51:13 +0330

    feat: add replay api for list

---
+++ src/pages/api/v1/apps/[app_id]/menus/[menu_id]/replay/index.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { pb } from "../../../../../../../../../pocketbase";
+import config from "next/config";
+const { publicRuntimeConfig } = config();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const { app_id, menu_id } = req.query;
+  const cookieStore = req.cookies;
+  const token =
+    (cookieStore["token"] as string) ||
+    req.headers.authorization?.replace("Bearer", "");
+
+  await pb.admins.authWithPassword(
+    publicRuntimeConfig.POCKETBASE_USER_NAME,
+    publicRuntimeConfig.POCKETBASE_PASSWORD
+  );
+
+  const paziresh24User = await axios.get(
+    "https://apigw.paziresh24.com/v1/auth/me",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const session = await pb.collection("replay").getFullList({
+    filter: `app="${app_id}" && menu="${menu_id}"`,
+  });
+
+  res.status(200).json(
+    session.map((item) => ({
+      id: item.id,
+      user: item.user,
+      device: item.device,
+      browser: item.browser,
+      events: item.events,
+    }))
+  );
+}
