770bdab2489221461b206f411c4deac883323699
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-07-14 11:44:46 +0330

    feat: add analytics api

--- src/lib/bridge/activeUsersLog.ts
+++ src/lib/bridge/activeUsersLog.ts
@@ -1,33 +1,17 @@
 import axios from "axios";
 import { detectBrowser, detectDevice } from "../utils";
 import { getState } from "./getState";
-import config from "next/config";
-const { publicRuntimeConfig } = config();
 
 export const activeUsersLog = async ({ menu, app }: any) => {
   try {
-    const user = await getState.user();
-    const provider = await getState.provider();
-
-    const options = {
-      method: "POST",
-      url: "https://hamdast-logging.darkube.app/api/v1/logstream/activeusers",
-      headers: {
-        Authorization: `Basic ${publicRuntimeConfig.HAMDAST_LOGGING_TOKEN}`,
-        "Content-Type": "application/json",
+    axios.post(
+      `/api/v1/apps/${app}/menus/${menu}/analytics`,
+      {
+        browser: detectBrowser(),
+        device: detectDevice(),
+        action: "activeusers",
       },
-      data: [
-        {
-          browser: detectBrowser(),
-          device: detectDevice(),
-          app: app,
-          menu: menu,
-          user_id: user.id,
-          job_title: provider?.job_title ?? "normal",
-        },
-      ],
-    };
-
-    axios.request(options);
+      { withCredentials: true }
+    );
   } catch (error) {}
 };
---
+++ src/pages/api/v1/apps/[app_id]/menus/[menu_id]/analytics.ts
@@ -0,0 +1,71 @@
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import config from "next/config";
+import NextCors from "nextjs-cors";
+const { publicRuntimeConfig } = config();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: new RegExp(".paziresh24."),
+    preflightContinue: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  });
+
+  if (req.method === "POST") {
+    const { app_id, menu_id } = req.query;
+    const { browser, device, action, duration } = req.body;
+    const cookieStore = req.cookies;
+    const token =
+      (cookieStore["token"] as string) ||
+      req.headers.authorization?.replace("Bearer", "");
+
+    const paziresh24User = await axios.get(
+      "https://apigw.paziresh24.com/v1/auth/me",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const user = paziresh24User.data?.users[0];
+    const paziresh24Provider = await axios.get(
+      `https://apigw.paziresh24.com/v1/providers?user_id=${user.id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const provider = paziresh24Provider.data?.providers[0];
+
+    const options = {
+      method: "POST",
+      url: `https://hamdast-logging.darkube.app/api/v1/logstream/${action}`,
+      headers: {
+        Authorization: `Basic ${publicRuntimeConfig.HAMDAST_LOGGING_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      data: [
+        {
+          browser,
+          device,
+          app: app_id,
+          menu: menu_id,
+          user_id: user.id,
+          job_title: provider?.job_title ?? "normal",
+          duration,
+        },
+      ],
+    };
+
+    await axios.request(options);
+
+    res.status(204).json({});
+  }
+}
