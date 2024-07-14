cf7c273f87659dc7a30ed5cbbd5f00e8b4662b89
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-07-14 12:33:52 +0330

    feat: add analytics get api

--- src/pages/api/v1/apps/[app_id]/analytics.ts
+++ src/pages/api/v1/apps/[app_id]/analytics.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 import config from "next/config";
 import NextCors from "nextjs-cors";
-import { pb } from "../../../../../../../../pocketbase";
+import { pb } from "../../../../../../pocketbase";
 const { publicRuntimeConfig } = config();
 
 function getDateDaysAgo(daysAgo: string) {
@@ -32,7 +32,7 @@ export default async function handler(
     optionsSuccessStatus: 200,
     credentials: true,
   });
-  const { app_id, menu_id, days_ago } = req.query;
+  const { app_id, days_ago } = req.query;
   const cookieStore = req.cookies;
   const token =
     (cookieStore["token"] as string) ||
@@ -72,7 +72,7 @@ export default async function handler(
         "Content-Type": "application/json",
       },
       data: {
-        query: `select * from activeusers where app = '${app_id}' and menu = '${menu_id}'`,
+        query: `select * from activeusers where app = '${app_id}'`,
         startTime: `${getDateDaysAgo(days_ago as string)}T00:00:00+00:00`,
         endTime: new Date().toISOString(),
       },
@@ -81,44 +81,5 @@ export default async function handler(
     const queryData = await axios.request(options);
 
     return res.status(200).json(queryData?.data ?? []);
-  }
-
-  if (req.method === "POST") {
-    const { browser, device, action, duration } = req.body;
-
-    const paziresh24Provider = await axios.get(
-      `https://apigw.paziresh24.com/v1/providers?user_id=${user.id}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    const provider = paziresh24Provider.data?.providers[0];
-
-    const options = {
-      method: "POST",
-      url: `https://hamdast-logging.darkube.app/api/v1/logstream/${action}`,
-      headers: {
-        Authorization: `Basic ${publicRuntimeConfig.HAMDAST_LOGGING_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      data: [
-        {
-          browser,
-          device,
-          app: app_id,
-          menu: menu_id,
-          user_id: user.id?.toString(),
-          job_title: provider?.job_title ?? "normal",
-          duration,
-        },
-      ],
-    };
-
-    await axios.request(options);
-
-    res.status(204).json({});
   }
 }
