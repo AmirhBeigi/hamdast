9a91ed778cb8e7cc457544e2de4012966e47ff71
plasmic-app <github-action@actions-template-sync.noreply.github.com>
2024-08-01 01:13:15 +0000

    chore(template): merge template changes :up:
    Signed-off-by: plasmic-app <github-action@actions-template-sync.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   trailingSlash: true,
-  publicRuntimeConfig: {
-    POCKETBASE_USER_NAME: process.env.POCKETBASE_USER_NAME,
-    POCKETBASE_PASSWORD: process.env.POCKETBASE_PASSWORD,
-    HAMDAST_LOGGING_TOKEN: process.env.HAMDAST_LOGGING_TOKEN,
-  },
 };
 
 module.exports = nextConfig;
