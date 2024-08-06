86d582abbab1b4cec5b7d91381796914dd6d239c
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-08-06 15:59:35 +0330

    chore: remove zlib

--- src/pages/api/v1/apps/[app_id]/menus/[menu_id]/replay/[replay_id].ts
+++ src/pages/api/v1/apps/[app_id]/menus/[menu_id]/replay/[replay_id].ts
@@ -4,26 +4,26 @@ import { pb } from "../../../../../../../../../pocketbase";
 import config from "next/config";
 const { publicRuntimeConfig } = config();
 import NextCors from "nextjs-cors";
-import zlib from "zlib";
+// import zlib from "zlib";
 
-function optimizeJSON(data: any) {
-  let minifiedData = JSON.stringify(data);
-  return new Promise((resolve, reject) => {
-    zlib.gzip(minifiedData, (err: any, buffer: any) => {
-      if (err) reject(err);
-      resolve(buffer);
-    });
-  });
-}
+// function optimizeJSON(data: any) {
+//   let minifiedData = JSON.stringify(data);
+//   return new Promise((resolve, reject) => {
+//     zlib.gzip(minifiedData, (err: any, buffer: any) => {
+//       if (err) reject(err);
+//       resolve(buffer);
+//     });
+//   });
+// }
 
-function deOptimizeJSON(data: any) {
-  return new Promise((resolve, reject) => {
-    zlib.unzip(data, (err: any, buffer: any) => {
-      if (err) reject(err);
-      resolve(JSON.parse(buffer));
-    });
-  });
-}
+// function deOptimizeJSON(data: any) {
+//   return new Promise((resolve, reject) => {
+//     zlib.unzip(data, (err: any, buffer: any) => {
+//       if (err) reject(err);
+//       resolve(JSON.parse(buffer));
+//     });
+//   });
+// }
 
 export default async function handler(
   req: NextApiRequest,
@@ -66,65 +66,65 @@ export default async function handler(
   if (req.method === "POST") {
     const { browser, device, user, events } = req.body;
 
-    const compressed = await optimizeJSON(events);
+    // const compressed = await optimizeJSON(events);
 
-    await pb
-      .collection("replay_session")
-      .getOne(replay_id as string)
-      .catch(async () => {
-        const data = {
-          id: replay_id,
-          app: app_id,
-          menu: menu_id,
-          browser: browser,
-          device: device,
-          user: user,
-        };
-        await pb.collection("replay_session").create(data);
-        const options = {
-          method: "POST",
-          url: `https://hamdast-logging.darkube.app/api/v1/logstream/replayevents`,
-          headers: {
-            Authorization: `Basic ${publicRuntimeConfig.HAMDAST_LOGGING_TOKEN}`,
-            "Content-Type": "application/json",
-          },
-          data: [
-            {
-              id: replay_id,
-              events: compressed,
-            },
-          ],
-        };
+    // await pb
+    //   .collection("replay_session")
+    //   .getOne(replay_id as string)
+    //   .catch(async () => {
+    //     const data = {
+    //       id: replay_id,
+    //       app: app_id,
+    //       menu: menu_id,
+    //       browser: browser,
+    //       device: device,
+    //       user: user,
+    //     };
+    //     await pb.collection("replay_session").create(data);
+    //     const options = {
+    //       method: "POST",
+    //       url: `https://hamdast-logging.darkube.app/api/v1/logstream/replayevents`,
+    //       headers: {
+    //         Authorization: `Basic ${publicRuntimeConfig.HAMDAST_LOGGING_TOKEN}`,
+    //         "Content-Type": "application/json",
+    //       },
+    //       data: [
+    //         {
+    //           id: replay_id,
+    //           events: compressed,
+    //         },
+    //       ],
+    //     };
 
-        await axios.request(options);
-      })
-      .then(async () => {
-        const data = {
-          id: replay_id,
-          app: app_id,
-          menu: menu_id,
-          browser: browser,
-          device: device,
-          user: user,
-        };
-        await pb.collection("replay_session").update(replay_id as string, data);
-        const options = {
-          method: "PUT",
-          url: `https://hamdast-logging.darkube.app/api/v1/logstream/replayevents`,
-          headers: {
-            Authorization: `Basic ${publicRuntimeConfig.HAMDAST_LOGGING_TOKEN}`,
-            "Content-Type": "application/json",
-          },
-          data: [
-            {
-              id: replay_id,
-              events: compressed,
-            },
-          ],
-        };
+    //     await axios.request(options);
+    //   })
+    //   .then(async () => {
+    //     const data = {
+    //       id: replay_id,
+    //       app: app_id,
+    //       menu: menu_id,
+    //       browser: browser,
+    //       device: device,
+    //       user: user,
+    //     };
+    //     await pb.collection("replay_session").update(replay_id as string, data);
+    //     const options = {
+    //       method: "PUT",
+    //       url: `https://hamdast-logging.darkube.app/api/v1/logstream/replayevents`,
+    //       headers: {
+    //         Authorization: `Basic ${publicRuntimeConfig.HAMDAST_LOGGING_TOKEN}`,
+    //         "Content-Type": "application/json",
+    //       },
+    //       data: [
+    //         {
+    //           id: replay_id,
+    //           events: compressed,
+    //         },
+    //       ],
+    //     };
 
-        await axios.request(options);
-      });
+    //     await axios.request(options);
+    //   });
 
     return res.status(204).end();
   }
