87aa0e5177e7995ee746a2497107c8b86c433192
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-08-06 15:48:44 +0330

    feat: add check key avalable

---
+++ src/pages/api/v1/apps/key/index.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { pb } from "../../../../../../pocketbase";
+import config from "next/config";
+const { publicRuntimeConfig } = config();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  pb.autoCancellation(false);
+  const cookieStore = req.cookies;
+  const { key } = req.query;
+  const token =
+    (cookieStore["token"] as string) ||
+    req.headers.authorization?.replace("Bearer", "");
+  await pb.admins.authWithPassword(
+    publicRuntimeConfig.POCKETBASE_USER_NAME,
+    publicRuntimeConfig.POCKETBASE_PASSWORD
+  );
+  const paziresh24User = await axios.get(
+    "https://apigw.paziresh24.com/v1/auth/me",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const user = paziresh24User.data?.users[0];
+  const record = await pb
+    .collection("users")
+    .getFirstListItem(`paziresh24_user_id="${user.id}"`, {
+      expand: "role",
+    });
+
+  if (req.method === "GET") {
+    const app = await pb.collection("apps").getFirstListItem(`key = '${key}'`);
+    if (app?.id) {
+      return res.status(400).json({});
+    }
+    return res.status(200).json({});
+  }
+}
