784c21c3411c73c0225ff8ccdf99a0f2d49062af
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-08-10 14:28:28 +0330

    feat: add notification api

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ const nextConfig = {
     POCKETBASE_USER_NAME: process.env.POCKETBASE_USER_NAME,
     POCKETBASE_PASSWORD: process.env.POCKETBASE_PASSWORD,
     HAMDAST_LOGGING_TOKEN: process.env.HAMDAST_LOGGING_TOKEN,
+    NAJVA_API_KEY: process.env.NAJVA_API_KEY,
+    NAJVA_TOKEN: process.env.NAJVA_TOKEN,
   },
 };
 
---
+++ src/pages/api/v1/apps/[app_id]/notifications/trigger/index.ts
@@ -0,0 +1,64 @@
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { pb } from "../../../../../../../../pocketbase";
+import config from "next/config";
+const { publicRuntimeConfig } = config();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  pb.autoCancellation(false);
+  const cookieStore = req.cookies;
+  const { app_id } = req.query;
+  const token =
+    (cookieStore["token"] as string) ||
+    req.headers.authorization?.replace("Bearer", "");
+  await pb.admins.authWithPassword(
+    publicRuntimeConfig.POCKETBASE_USER_NAME,
+    publicRuntimeConfig.POCKETBASE_PASSWORD
+  );
+  const paziresh24User = await axios.get(
+    "https://apigw.paziresh24.com/v1/auth/me",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const user = paziresh24User.data?.users[0];
+  const record = await pb
+    .collection("users")
+    .getFirstListItem(`paziresh24_user_id="${user.id}"`, {
+      expand: "role",
+    });
+
+  if (req.method === "POST") {
+    const { ...rest } = req.body;
+
+    try {
+      const data = await axios.post(
+        "https://app.najva.com/notification/api/v1/notifications/",
+        {
+          api_key: publicRuntimeConfig.NAJVA_API_KEY,
+          ...rest,
+        },
+        {
+          headers: {
+            authorization: `Token ${publicRuntimeConfig.NAJVA_TOKEN}`,
+          },
+        }
+      );
+
+      return res.status(200).json({
+        ...data.data,
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return res.status(error?.status ?? 500).json({
+          ...(error?.response?.data ?? {}),
+        });
+      }
+    }
+  }
+}
