de13323dfeb4cd02fb5e6d601dfc8db2b14ce8a9
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-08-10 14:32:04 +0330

    feat: add error handling for notification

--- src/pages/api/v1/apps/[app_id]/notifications/trigger/index.ts
+++ src/pages/api/v1/apps/[app_id]/notifications/trigger/index.ts
@@ -18,6 +18,7 @@ export default async function handler(
     publicRuntimeConfig.POCKETBASE_USER_NAME,
     publicRuntimeConfig.POCKETBASE_PASSWORD
   );
+
   const paziresh24User = await axios.get(
     "https://apigw.paziresh24.com/v1/auth/me",
     {
@@ -27,38 +28,47 @@ export default async function handler(
     }
   );
   const user = paziresh24User.data?.users[0];
-  const record = await pb
-    .collection("users")
-    .getFirstListItem(`paziresh24_user_id="${user.id}"`, {
-      expand: "role",
-    });
-
-  if (req.method === "POST") {
-    const { ...rest } = req.body;
-
-    try {
-      const data = await axios.post(
-        "https://app.najva.com/notification/api/v1/notifications/",
-        {
-          api_key: publicRuntimeConfig.NAJVA_API_KEY,
-          ...rest,
-        },
-        {
-          headers: {
-            authorization: `Token ${publicRuntimeConfig.NAJVA_TOKEN}`,
-          },
-        }
-      );
-
-      return res.status(200).json({
-        ...data.data,
+  try {
+    const record = await pb
+      .collection("users")
+      .getFirstListItem(`paziresh24_user_id="${user.id}"`, {
+        expand: "role",
       });
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return res.status(error?.status ?? 500).json({
-          ...(error?.response?.data ?? {}),
+
+    if (req.method === "POST") {
+      const { ...rest } = req.body;
+
+      try {
+        const data = await axios.post(
+          "https://app.najva.com/notification/api/v1/notifications/",
+          {
+            api_key: publicRuntimeConfig.NAJVA_API_KEY,
+            ...rest,
+          },
+          {
+            headers: {
+              authorization: `Token ${publicRuntimeConfig.NAJVA_TOKEN}`,
+            },
+          }
+        );
+
+        return res.status(200).json({
+          ...data.data,
         });
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          return res.status(error?.status ?? 500).json({
+            ...(error?.response?.data ?? {}),
+          });
+        }
       }
     }
+    return res.status(405).json({
+      message: "use POST method",
+    });
+  } catch (error) {
+    return res.status(401).json({
+      message: "Authentication credentials were not provided.",
+    });
   }
 }
