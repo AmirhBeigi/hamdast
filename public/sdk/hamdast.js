52efac03331b1e33a54b1b590e7e092708768d95
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-09-04 12:38:50 +0330

    fix

--- public/sdk/hamdast.js
+++ public/sdk/hamdast.js
@@ -1,1 +1,132 @@
-function hamdastCommunication({ clientKey: e, event: a, data: t, promise: n = !1 }) { const i = { hamdast: { clientKey: e, host: window.location.origin, event: a, data: t } }; if (n) return hamdastPostMessagePromise(i); window.parent.postMessage(i, "*") } function hamdastRrwebFireWhenReady() { void 0 !== window?.rrweb?.record ? window.hamdast?.replay?.record && "function" == typeof window.hamdast?.replay?.record && window.hamdast?.replay?.record?.() : setTimeout((() => hamdastRrwebFireWhenReady(window?.rrweb?.record, window.hamdast?.replay?.record)), 100) } !function () { var e = document.getElementsByTagName("head")[0], a = document.createElement("script"); a.async = !0, a.id = "rrweb-hamdast-script", a.src = "https://hamdast.paziresh24.com/static/js/scripts/rrweb-all.min.js", e.appendChild(a) }(); const hamdastPostMessagePromise = e => { const a = Date.now(); return window.parent.postMessage({ hamdast: { ...e.hamdast, hash_id: a } }, "*"), new Promise(((e, t) => { window.addEventListener("message", (function (t) { t.data?.hamdast?.hash_id === a && e(t.data?.hamdast?.data) })), setTimeout((() => { t() }), 3e4) })) }; window.hamdast = { initialize({ clientKey: e = '' }) { window.hamdast.clientKey = e, hamdastCommunication({ clientKey: e, event: "HAMDAST_INITIALIZE" }), hamdastRrwebFireWhenReady() }, clientKey: null, getState: e => hamdastCommunication({ clientKey: window.hamdast.clientKey, promise: !0, event: "HAMDAST_GET_STATE", data: { state: e } }), getSessionToken: () => hamdastCommunication({ clientKey: window.hamdast.clientKey, promise: !0, event: "HAMDAST_GET_SESSION_TOKEN" }), replay: { record() { rrweb.record({ emit(e) { window.hamdast?.replay?.events?.push?.(e) }, sampling: { input: "last", scroll: 150, media: 800 } }), setInterval(window.hamdast?.replay?.save, 1e4) }, save() { hamdastCommunication({ clientKey: window.hamdast.clientKey, event: "HAMDAST_REPLAY_SAVE", data: { events: window.hamdast?.replay?.events } }) }, events: [] }, openLink: ({ url: e }) => window.self !== window.top ? hamdastCommunication({ clientKey: window.hamdast.clientKey, event: "HAMDAST_OPEN_LINK", data: { url: e } }) : location.assign(e) };
+(function () {
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.createElement("script");
+    script.async = true;
+    script.id = "rrweb-hamdast-script";
+    script.src =
+        "https://hamdast.paziresh24.com/static/js/scripts/rrweb-all.min.js";
+    head.appendChild(script);
+})();
+
+function hamdastCommunication({ clientKey, event, data, promise = false }) {
+    const message = {
+        hamdast: {
+            clientKey,
+            host: window.location.origin,
+            event,
+            data,
+        },
+    };
+    if (promise) {
+        return hamdastPostMessagePromise(message);
+    }
+    window.parent.postMessage(message, "*");
+}
+
+function hamdastRrwebFireWhenReady() {
+    if (typeof window?.rrweb?.record != "undefined") {
+        if (
+            window.hamdast?.replay?.record &&
+            typeof window.hamdast?.replay?.record === "function"
+        ) {
+            window.hamdast?.replay?.record?.();
+        }
+    } else {
+        setTimeout(
+            () =>
+                hamdastRrwebFireWhenReady(
+                    window?.rrweb?.record,
+                    window.hamdast?.replay?.record
+                ),
+            100
+        );
+    }
+}
+
+const hamdastPostMessagePromise = (message) => {
+    const hash_id = Date.now();
+
+    window.parent.postMessage(
+        {
+            hamdast: {
+                ...message.hamdast,
+                hash_id,
+            },
+        },
+        "*"
+    );
+
+    return new Promise((resolve, reject) => {
+        window.addEventListener("message", function (e) {
+            if (e.data?.hamdast?.hash_id === hash_id) {
+                resolve(e.data?.hamdast?.data);
+            }
+        });
+        setTimeout(() => {
+            reject();
+        }, 30000);
+    });
+};
+
+window.hamdast = {
+    initialize({ clientKey = '' } = { clientKey: '' }) {
+        window.hamdast.clientKey = clientKey;
+        hamdastCommunication({ clientKey, event: "HAMDAST_INITIALIZE" });
+        hamdastRrwebFireWhenReady();
+    },
+    clientKey: null,
+    getState(state) {
+        return hamdastCommunication({
+            clientKey: window.hamdast.clientKey,
+            promise: true,
+            event: "HAMDAST_GET_STATE",
+            data: {
+                state,
+            },
+        });
+    },
+    getSessionToken() {
+        return hamdastCommunication({
+            clientKey: window.hamdast.clientKey,
+            promise: true,
+            event: "HAMDAST_GET_SESSION_TOKEN",
+        });
+    },
+    replay: {
+        record() {
+            rrweb.record({
+                emit(event) {
+                    window.hamdast?.replay?.events?.push?.(event);
+                },
+                sampling: {
+                    input: "last",
+                    scroll: 150,
+                    media: 800,
+                },
+            });
+
+            // save events every 10 seconds
+            setInterval(window.hamdast?.replay?.save, 10 * 1000);
+        },
+        save() {
+            hamdastCommunication({
+                clientKey: window.hamdast.clientKey,
+                event: "HAMDAST_REPLAY_SAVE",
+                data: { events: window.hamdast?.replay?.events },
+            });
+        },
+        events: [],
+    },
+    openLink({ url }) {
+        if (window.self !== window.top) {
+            return hamdastCommunication({
+                clientKey: window.hamdast.clientKey,
+                event: "HAMDAST_OPEN_LINK",
+                data: {
+                    url,
+                },
+            });
+        }
+        return location.assign(url);
+    },
+};
