0078af7f44fb91b88a119d3dd5f506db5e71dabc
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-03-24 16:46:45 +0330

    feat: add post and delete widget

--- src/pages/api/v1/widgets.ts
+++ src/pages/api/v1/widgets.ts
@@ -1,29 +1,157 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { pb } from "../../../../pocketbase";
 import config from "next/config";
+import axios from "axios";
+import NextCors from "nextjs-cors";
 const { publicRuntimeConfig } = config();
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const { id } = req.query;
-
-  const data = await pb.collection("profile_widgets").getFullList({
-    filter: `profile_id="${id}"`,
-    expand: "widget",
-    headers: {
-      x_token: publicRuntimeConfig.HAMDAST_TOKEN,
-    },
+  pb.autoCancellation(false);
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    origin: new RegExp(".paziresh24."),
+    preflightContinue: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+    headers: [
+      "X-CSRF-Token",
+      "x-xsrf-token",
+      "X-Requested-With",
+      "Accept",
+      "Accept-Version",
+      "Content-Length",
+      "Content-MD5",
+      "Content-Type",
+      "Date",
+      "X-Api-Version",
+      "token",
+      "Authorization",
+    ],
   });
 
-  return res.status(200).json(
-    data?.map((item) => ({
-      id: item?.widget,
-      plasmic_component_id: item.expand?.widget?.plasmic_component_id,
-      plasmic_project_id: item?.expand?.widget?.plasmic_project_id,
-      placement: item?.expand?.widget?.placement,
-      data_endpoint: item?.expand?.widget?.data_endpoint ?? null,
-    }))
-  );
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  if (req.method == "GET") {
+    const { id } = req.query;
+
+    const data = await pb.collection("profile_widgets").getFullList({
+      filter: `profile_id="${id}"`,
+      expand: "widget",
+      headers: {
+        x_token: publicRuntimeConfig.HAMDAST_TOKEN,
+      },
+    });
+
+    return res.status(200).json(
+      data?.map((item) => ({
+        id: item?.widget,
+        plasmic_component_id: item.expand?.widget?.plasmic_component_id,
+        plasmic_project_id: item?.expand?.widget?.plasmic_project_id,
+        placement: item?.expand?.widget?.placement,
+        data_endpoint: item?.expand?.widget?.data_endpoint ?? null,
+      }))
+    );
+  }
+
+  if (req.method == "POST") {
+    const { app_id } = req.body;
+
+    const cookieStore = req.cookies;
+    const token =
+      (cookieStore["token"] as string) ||
+      req.headers.authorization?.replace("Bearer", "");
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Authentication credentials were not provided.",
+      });
+    }
+
+    const profile = await axios.get(
+      "https://api.paziresh24.com/V1/doctor/profile",
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const widget = await pb
+      .collection("widgets")
+      .getFirstListItem(`app = "${app_id}"`, {
+        headers: {
+          x_token: publicRuntimeConfig.HAMDAST_TOKEN,
+        },
+      });
+
+    try {
+      await pb.collection("profile_widgets").create(
+        {
+          profile_id: profile?.data?.data?.id,
+          widget: widget.id,
+        },
+        {
+          headers: {
+            x_token: publicRuntimeConfig.HAMDAST_TOKEN,
+          },
+        }
+      );
+      return res.status(200).json({});
+    } catch (error) {
+      return res.status(400).json({
+        message: "قبلا این ویجت اضافه شده است.",
+      });
+    }
+  }
+
+  if (req.method == "DELETE") {
+    const { app_id } = req.body;
+
+    const cookieStore = req.cookies;
+    const token =
+      (cookieStore["token"] as string) ||
+      req.headers.authorization?.replace("Bearer", "");
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Authentication credentials were not provided.",
+      });
+    }
+
+    const profile = await axios.get(
+      "https://api.paziresh24.com/V1/doctor/profile",
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const profile_id = profile?.data?.data?.id;
+
+    try {
+      const widget = await pb
+        .collection("profile_widgets")
+        .getFirstListItem(`profile_id = "${profile_id}"`, {
+          headers: {
+            x_token: publicRuntimeConfig.HAMDAST_TOKEN,
+          },
+        });
+
+      await pb.collection("profile_widgets").delete(widget.id, {
+        headers: {
+          x_token: publicRuntimeConfig.HAMDAST_TOKEN,
+        },
+      });
+      return res.status(200).json({});
+    } catch (error) {
+      return res.status(400).json({
+        message: "ویجتی وجود ندارد.",
+      });
+    }
+  }
 }
