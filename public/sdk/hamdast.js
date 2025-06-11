2837ffaaf1f58cd7c6ca575861e64b53c9013b62
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-06-11 19:13:26 +0330

    feat: add clarity script to hamdast sdk

--- public/sdk/hamdast.js
+++ public/sdk/hamdast.js
@@ -108,3 +108,18 @@ window.hamdast = {
     },
   },
 };
+
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  if (!document.querySelector('script[src*="clarity.ms"]')) {
+    y.parentNode.insertBefore(t, y);
+  }
+})(window, document, "clarity", "script", "4zn9fqioi8");
