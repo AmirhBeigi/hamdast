5c7a41c239fb2bab8646b898f691593af6ab8900
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-08-21 07:49:43 +0330

    feat: add user-page api

---
+++ src/pages/api/v1/apps/[app_id]/features/user-page.ts
@@ -0,0 +1,106 @@
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { pb } from "../../../../../../../pocketbase";
+import config from "next/config";
+import { ClientResponseError } from "pocketbase";
+const { publicRuntimeConfig } = config();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  pb.autoCancellation(false);
+  const cookieStore = req.cookies;
+  const { app_id } = req.query;
+  const token =
+    (cookieStore["token"] as string) ||
+    req.headers.authorization?.replace("Bearer", "");
+  await pb.admins.authWithPassword(
+    publicRuntimeConfig.POCKETBASE_USER_NAME,
+    publicRuntimeConfig.POCKETBASE_PASSWORD
+  );
+  const paziresh24User = await axios.get(
+    "https://apigw.paziresh24.com/v1/auth/me",
+    {
+      headers: {
+        Authorization: `Bearer ${token?.trim()}`,
+      },
+    }
+  );
+  const user = paziresh24User.data?.users[0];
+
+  const record = await pb
+    .collection("users")
+    .getFirstListItem(`paziresh24_user_id="${user.id}"`, {
+      expand: "role",
+    });
+
+  const app = await pb
+    .collection("apps")
+    .getFirstListItem(`collaborators~'${record.id}' && id = '${app_id}'`);
+
+  if (req.method == "GET") {
+    const page = await pb
+      .collection("pages")
+      .getFirstListItem(`app="${app_id}" && key="launcher"`);
+
+    res.status(200).json({
+      id: page.id,
+      key: page.key,
+      embed_src: page.embed_src,
+      display_name_fa: app?.display_name_fa,
+      description: app?.description,
+    });
+  }
+
+  if (req.method == "POST") {
+    const { embed_src, display_name_fa, description } = req.body;
+
+    try {
+      await pb.collection("apps").update(app.id, {
+        display_name_fa: display_name_fa,
+        description: description,
+      });
+
+      const page = await pb
+        .collection("pages")
+        .getFirstListItem(`app="${app_id}" && key="launcher"`);
+
+      if (page?.id) {
+        await pb.collection("pages").update(page.id, {
+          key: "launcher",
+          name_fa: display_name_fa,
+          embed_src: embed_src,
+          app: app_id,
+          layout: {
+            show_appbar: true,
+            show_bottom_navigation: true,
+            show_footer: false,
+            show_header: false,
+          },
+          parameters: [],
+          is_protected_route: true,
+        });
+      } else {
+        await pb.collection("pages").create({
+          key: "launcher",
+          name_fa: display_name_fa,
+          embed_src: embed_src,
+          app: app_id,
+          layout: {
+            show_appbar: true,
+            show_bottom_navigation: true,
+            show_footer: false,
+            show_header: false,
+          },
+          parameters: [],
+          is_protected_route: true,
+        });
+      }
+      res.status(200).json({});
+    } catch (error) {
+      const err = error as ClientResponseError;
+      res.status(err.status).json({ message: err.message });
+    }
+  }
+}
