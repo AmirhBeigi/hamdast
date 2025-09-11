92a4bb25c400569bf22b40098a29e67f36ca3418
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-09-11 16:14:29 +0330

    feat: change external collection uri to internal

--- pocketbase.ts
+++ pocketbase.ts
@@ -1,6 +1,8 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase("https://hamdast-collection.paziresh24.com");
+export const pb = new PocketBase(
+  "http://hamdast-collection.paziresh24-cloud-front.svc:8090"
+);
 
 export const notificationPB = new PocketBase(
   "https://notification-users.darkube.app"
