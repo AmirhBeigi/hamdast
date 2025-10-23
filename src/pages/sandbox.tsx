3ad5eadd9b64be287cb6decde5784fcad2ef306a
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-10-23 10:50:38 +0330

    fix: update import paths for GlobalContextsProvider and PlasmicBuild components in sandbox and authorization pages

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -2,9 +2,9 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
-import GlobalContextsProvider from "../../../../../components/plasmic/hamdast/PlasmicGlobalContextsProvider";
+import GlobalContextsProvider from "../../components/plasmic/hamdast/PlasmicGlobalContextsProvider";
 
-import { PlasmicBuildSandbox } from "../../../../../components/plasmic/hamdast/PlasmicBuildSandbox";
+import { PlasmicBuildSandbox } from "../../components/plasmic/hamdast/PlasmicBuildSandbox";
 import { useRouter } from "next/router";
 
 function BuildSandbox() {
