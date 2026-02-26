cd9fa865d7e71713206e338713062a789be37653
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2026-02-26 11:21:44 +0330

    feat: add subscription API handler for app management
    - Implemented a new API endpoint for managing subscriptions associated with specific apps.
    - Added CORS support to handle cross-origin requests.
    - Included authentication checks using API keys and user roles.
    - Enabled fetching of subscription data based on date range and status.
    - Handled various HTTP methods with appropriate responses for better API usability.

---
+++ src/pages/api/v1/apps/[app_id]/subscriptions/index.ts
@@ -0,0 +1,114 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { pb } from "../../../../../../../pocketbase";
+import config from "next/config";
+import axios from "axios";
+import NextCors from "nextjs-cors";
+const { publicRuntimeConfig } = config();
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) {
+    pb.autoCancellation(false);
+    await NextCors(req, res, {
+        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+        origin: "*",
+        preflightContinue: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+        headers: [
+            "X-CSRF-Token",
+            "x-xsrf-token",
+            "X-Requested-With",
+            "Accept",
+            "Accept-Version",
+            "Content-Length",
+            "Content-MD5",
+            "Content-Type",
+            "Date",
+            "X-Api-Version",
+            "token",
+            "Authorization",
+            "X-Api-KEY",
+            "x-api-key",
+        ],
+    });
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
+    const { app_id } = req.query;
+
+    const apiKey = req.headers?.["x-api-key"];
+    if (!apiKey) {
+        return res.status(401).json({
+            message: "Authentication credentials were not provided.",
+        });
+    }
+
+    let record;
+    try {
+        record = await pb
+            .collection("users")
+            .getFirstListItem(`api_key="${apiKey}"`, {
+                expand: "role",
+                cache: "force-cache",
+                headers: {
+                    x_token: publicRuntimeConfig.HAMDAST_TOKEN,
+                },
+            });
+    } catch (error) {
+        return res.status(401).json({
+            message: "Authentication credentials were not provided.",
+        });
+    }
+
+    if (!record) {
+        return res.status(401).json({
+            message: "Authentication credentials were not provided.",
+        });
+    }
+
+    let app;
+    try {
+        app = await pb
+            .collection("apps")
+            .getFirstListItem(`collaborators~"${record.id}" && key = "${app_id}"`, {
+                headers: {
+                    x_token: publicRuntimeConfig.HAMDAST_TOKEN,
+                },
+            });
+    } catch (error) {
+        return res.status(403).json({
+            message: "You do not have permission to access this app.",
+        });
+    }
+
+    if (req.method == "GET") {
+        const { start_date, end_date, status } = req.query;
+
+
+        try {
+            const subscriptions = await axios.get(`https://hamdast-workflow.paziresh24.com/webhook/3a23b6bd-41d6-4655-a25d-711630853b59/v1/hamdast/admin/apps/${app?.key}/subscribers`, {
+                params: {
+                    start_date,
+                    end_date,
+                    ...(status && { is_active: status === "active" }),
+                }
+            });
+
+            return res.status(200).json(
+                subscriptions?.data
+            );
+        } catch (error) {
+            return res.status(404).json({
+                message: "Subscriptions not found",
+            });
+        }
+    }
+
+    return res.status(405).json({
+        message: "Method Not Allowed",
+    });
+}
